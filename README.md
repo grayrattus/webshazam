c0b578632b39f3a2963c9752f8e32a17244345a3
grayrattus <arturdziedziczak0@gmail.com>
2021-05-05 20:01:08 +0200

    Add basic screenshot functionality

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-g++ webshazam.c -o webshazam $(pkg-config --libs --cflags webkit2gtk-4.0 webkit2gtk-web-extension-4.0 gio-2.0)
+g++-10 webshazam.c -o webshazam $(pkg-config --libs --cflags webkit2gtk-4.0 webkit2gtk-web-extension-4.0 gio-2.0)
--- webshazam.c
+++ webshazam.c
@@ -2,9 +2,30 @@
 #include <stddef.h>
 #include <unistd.h>
 
+#include <future>
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
+static void
+getScreenshotFunction(GObject *object,
+                     GAsyncResult *result,
+                     gpointer data)
+{
+    WebKitWebView * webview = (WebKitWebView*)data;
+    GError* error = NULL;
+	cairo_surface_t* surface = webkit_web_view_get_snapshot_finish(webview, result, &error);
+	cairo_surface_write_to_png(surface, "/tmp/test.png");
+}
+
+static int startScreenshot(gpointer webView) {
+    webkit_web_view_get_snapshot((WebKitWebView*)webView,
+                                 WEBKIT_SNAPSHOT_REGION_FULL_DOCUMENT,
+                                 WEBKIT_SNAPSHOT_OPTIONS_NONE,
+                                 NULL,
+                                 getScreenshotFunction,
+                                 (WebKitWebView*)webView);
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
@@ -21,11 +42,14 @@ int main(int argc, char* argv[])
             GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_container_add(GTK_CONTAINER(scrolledWindow), GTK_WIDGET(webView));
     gtk_container_add(GTK_CONTAINER(main_window), scrolledWindow);
-    webkit_web_view_load_uri(webView, "https://bbc.github.io/psammead/?path=/story/components-brand--without-brand-link");
+    webkit_web_view_load_uri(webView, "https://bbc.github.io/psammead/iframe.html?id=components-brand--without-brand-link&viewMode=story");
     gtk_widget_grab_focus(GTK_WIDGET(webView));
     gtk_widget_show_all(main_window);
 
+    g_timeout_add_seconds(5, startScreenshot, webView);
+
     gtk_main();
+
 
     return 0;
 }
