fee9cd128e7086fa4f75ac6b971fed13dd6f326a
grayrattus <arturdziedziczak0@gmail.com>
2021-05-08 15:52:21 +0200

    Run webshazam on my blog

--- screenshots.sh
+++ screenshots.sh
@@ -2,4 +2,4 @@ Xvfb :500 -screen 0 1280x1024x24 &
 Xvfb :501 -screen 0 1280x1024x24 &
 Xvfb :502 -screen 0 1280x1024x24 &
 Xvfb :503 -screen 0 1280x1024x24 &
-parallel -j 4 DISPLAY=:{2} ./webshazam -s ~/here{1}.png -u 'https://next--storybookjs.netlify.app/official-storybook/iframe.html?id=addons-backgrounds--with-gradient&args=&viewMode=story' ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ::: 500 501 502 503
+parallel -j 4 DISPLAY=:{2} ./webshazam -s ~/here{1}.png -u 'https://www.dziedziczak-artur.com/computers/bash-morse/beep-beep/' ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ::: 500 501 502 503
